6ae43c41a1e072836b8260c9141c73954f857c83
manu <manuel.jh.escalera@gmail.com>
2023-10-21 13:46:57 -0300

    commits endpoint

---
+++ app/api/commits/route.ts
@@ -0,0 +1,19 @@
+import { Octokit } from "octokit";
+
+export async function GET(request:Request) {
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_API_KEY
+    });
+    
+    const response = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+        owner: 'Manuel-JH-Escalera',
+        repo: 'github-commit-viewer',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+    return new Response(JSON.stringify(response.data), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
